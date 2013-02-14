139707de2952fc442be8974480d448b3341723f6
Stuart Watson <stuart.watson@unep-wcmc.org>
2013-02-14 14:49:05 +0000

    Fixed problem with install

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
-#apt-get -y update
-#apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-dev libyaml-dev git
-#cd /usr/local/src
-#wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p194.tar.gz
-#tar -xvzf ruby-1.9.3-p194.tar.gz
-#cd ruby-1.9.3-p194/
-#./configure --prefix=/usr/local
-#make
-#make install
-#rm /usr/local/src/ruby-1.9.3-p194.tar.gz
-#gem install chef ruby-shadow --no-ri --no-rdoc
-#git clone https://github.com/strtwtsn/LampServer.git /var/chef
-#sudo /usr/local/bin/chef-solo -c /var/chef/config/chefsolo.rb -j /var/chef/roles/lampserver.json
+!/bin/bash
+apt-get -y update
+apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-dev libyaml-dev git
+cd /usr/local/src
+wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p194.tar.gz
+tar -xvzf ruby-1.9.3-p194.tar.gz
+cd ruby-1.9.3-p194/
+./configure --prefix=/usr/local
+make
+make install
+rm /usr/local/src/ruby-1.9.3-p194.tar.gz
+gem install chef ruby-shadow --no-ri --no-rdoc
+git clone https://github.com/strtwtsn/LampServer.git /var/chef
+sudo /usr/local/bin/chef-solo -c /var/chef/config/chefsolo.rb -j /var/chef/roles/lampserver.json
 read -p "Enter new site name:" site
 cd /etc/apache2/sites-available
 wget https://raw.github.com/strtwtsn/LampServer/master/files/template
